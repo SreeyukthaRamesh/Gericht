0fcaf9091140f11b1d7196f82caf503e6429e9b0
Sreeyuktha Ramesh <124347343+SreeyukthaRamesh@users.noreply.github.com>
2024-01-11 19:36:39 +0530

    first commit

---
+++ src/container/Laurels/Laurels.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { images , data} from  '../../constants'
+import { SubHeading } from "../../components"
+import './Laurels.css';
+
+const Laurels = () => (
+  <div className='app__bg app__wrapper section_padding'>
+    
+  </div>
+);
+
+export default Laurels;
